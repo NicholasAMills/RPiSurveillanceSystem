910e4b26ff731a6f9c8bf4d6a2bdd0bf4a1b9dcd
Nick <nicholas_mills@outlook.com>
2020-01-17 21:58:55 -0600

    Updated instructions in  cred.py

--- cred.py
+++ cred.py
@@ -1,13 +1,18 @@
 '''
-    Class to hold username and password for email, along with recpient email as a QOL improvement
+    Class to hold username and password for email, along with recpient email as a QOL improvement.
+
+    NOTE: get_username() email MUST be a gmail account. get_recipient() can be any email address.
+    Basic idea is to send it to yourself. If you don't use gmail as your primary email, create one just for this sole
+    purpose and then make the recipient your primary email address. This is where you receive the photos from as
+    attachments.
 '''
 
 class Login():
     def get_username():
-        return 'your email here' # enter your email here inside the single quotes
+        return 'your gmail here' # enter a gmail account here inside the single quotes. This must be a gmail account.
 
     def get_password():
         return 'your email password here' # enter your email password here inside the single quotes
 
     def get_recipient():
-        return 'recipient email here' # enter reciever's email inside the single quotes
+        return 'recipient email here' # enter destination email inside the single quotes
