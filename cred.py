a63ccefe774a837a0703fa7a211426bb78b00183
NicholasAMills <37703788+NicholasAMills@users.noreply.github.com>
2020-01-17 21:14:28 -0600

    Update cred.py

--- cred.py
+++ cred.py
@@ -1,9 +1,9 @@
 class Login():
     def get_username():
-        return 'RPiSurveillance01@gmail.com' # enter your email here
+        return 'your email here' # enter your email here
 
     def get_password():
-        return '1C@n$33u' # enter your email password
+        return 'your password here' # enter your email password
 
     def get_recipient():
-        return 'nicholas_mills@outlook.com' # enter reciever's email
+        return 'recipient email here' # enter reciever's email
