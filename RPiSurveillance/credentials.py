fc9660c308b07970552f3afd3be5d7b23acc6a6d
NicholasAMills <37703788+NicholasAMills@users.noreply.github.com>
2020-01-17 23:13:25 -0600

    Add files via upload

---
+++ RPiSurveillance/credentials.py
@@ -0,0 +1,17 @@
+'''
+    Class to hold username and password for email, along with recipient email as a QOL improvement.
+    NOTE: get_username() email MUST be a gmail account. get_recipient() can be any email address.
+    Basic idea is to send it to yourself. If you don't use gmail as your primary email, create one just for this sole
+    purpose and then make the recipient your primary email address. This is where you receive the photos from as
+    attachments.
+'''
+
+class Login():
+    def get_username():
+        return 'gmail account here' # enter a gmail account here inside the single quotes. This must be a gmail account.
+
+    def get_password():
+        return 'gmail password here' # enter your gmail password here inside the single quotes
+
+    def get_recipient():
+        return 'receiving email here' # enter destination email inside the single quotes
