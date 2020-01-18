3f9cbf43a52665b9b81ca1ad0dce70c1c0f5eb6a
Nick <nicholas_mills@outlook.com>
2020-01-17 21:20:14 -0600

    QOL update for credentials and recipient email

---
+++ cred.py
@@ -0,0 +1,13 @@
+'''
+    Class to hold username and password for email, along with recpient email as a QOL improvement
+'''
+
+class Login():
+    def get_username():
+        return 'your email here' # enter your email here inside the single quotes
+
+    def get_password():
+        return 'your email password here' # enter your email password here inside the single quotes
+
+    def get_recipient():
+        return 'recipient email here' # enter reciever's email inside the single quotes
