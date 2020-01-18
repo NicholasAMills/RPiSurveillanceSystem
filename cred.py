d24711db5b8b04896de1c422269dabe70306b140
Nick <nicholas_mills@outlook.com>
2020-01-17 21:13:07 -0600

    added cred.py to github

---
+++ cred.py
@@ -0,0 +1,9 @@
+class Login():
+    def get_username():
+        return 'RPiSurveillance01@gmail.com' # enter your email here
+
+    def get_password():
+        return '1C@n$33u' # enter your email password
+
+    def get_recipient():
+        return 'nicholas_mills@outlook.com' # enter reciever's email
