a762e9b2956718cea49d77e26dcc661ef72f9484
NicholasAMills <37703788+NicholasAMills@users.noreply.github.com>
2020-01-17 21:15:36 -0600

    Delete cred.py

--- cred.py
+++
@@ -1,9 +0,0 @@
-class Login():
-    def get_username():
-        return 'your email here' # enter your email here
-
-    def get_password():
-        return 'your password here' # enter your email password
-
-    def get_recipient():
-        return 'recipient email here' # enter reciever's email
